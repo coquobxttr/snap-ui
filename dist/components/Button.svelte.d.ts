4cf73300ad413af4b377855e948c228ad560334c
Zainab <zainabnadeniji@gmail.com>
2025-06-10 21:01:25 +0100

    removed dist folder

--- dist/components/Button.svelte.d.ts
+++
@@ -1,7 +0,0 @@
-declare const Button: import("svelte").Component<{
-    bgColour: any;
-    children: any;
-    id: any;
-}, {}, "">;
-type Button = ReturnType<typeof Button>;
-export default Button;
--- dist/components/Dropdown.svelte.d.ts
+++
@@ -1,9 +0,0 @@
-declare const Dropdown: import("svelte").Component<{
-    targetId: any;
-    bgColour?: string;
-    placement?: string;
-    arrow?: boolean;
-    children: any;
-}, {}, "">;
-type Dropdown = ReturnType<typeof Dropdown>;
-export default Dropdown;

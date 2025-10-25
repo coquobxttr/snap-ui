eae786f5d1cf03d510568d57ef3395b4c1549cdd
Zainab <zainabnadeniji@gmail.com>
2025-10-25 12:56:54 +0100

    added trailingslash setting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
 	import { Snap } from '$lib/index.js';
 	import { onMount } from 'svelte';
 
